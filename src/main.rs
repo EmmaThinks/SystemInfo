11ff41959b5b837ba8fd6e59e9965e0285dd4a18
Emma B <EmmaThinks>
2026-02-16 11:29:30 -0600

    updated RAM section

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 use clearscreen;
-use sysinfo::System;
+use sysinfo::{Disk, System};
 
 fn main() {
     let mut sys = System::new_all();
-    let total_ram = sys.total_memory() / 1024 / 1024 / 1024;
+    let total_ram: f32 = (sys.total_memory() / 1024 / 1024 / 1024) as f32;
+    let total_swap = sys.total_swap() / 1024 / 1024 / 1024;
 
     loop {
         clearscreen::clear().expect("No se pudo limpiar la pantalla");
         sys.refresh_all();
-        //variables de recursos aqui
-
+        // VARIABLES:
         let used_ram = sys.used_memory() / 1024 / 1024 / 1024;
+        let used_swap = sys.used_swap() / 1024 / 1024 / 1024;
 
-        println!("INFORMACION DEL SISTEMA:\n");
+        println!("SYSTEM INFO 0.1:\n");
 
-        println!("Recursos:");
-        println!("RAM:\n   Total:{0}\n   Used:{1}", total_ram, used_ram);
-        println!("CPU:");
+        // GEN INFORMATION:
+
+        println!();
+
+        println!("Resources:");
+
+        // RAM MEMORY:
+        println!(
+            "RAM:
+
+    Total ========> [ {0} ]
+    Used =========> [ {1} ]
+    Swap Total ===> [ {2} ]
+    Swap Used ====> [ {3} ]
+    ",
+            total_ram, used_ram, total_swap, used_swap
+        );
+
+        // CPU USAGE & TOTAL CORES:
+        println!("CPU:\n   Total Cores:{}", sys.cpus().len());
         println!("Discos:");
-        println!("Recursos:");
+        println!("");
 
         std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
     }
