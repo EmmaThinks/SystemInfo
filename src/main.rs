b41301446c15bb9b430314718b5094d0c9fe1675
EmmaThinks <em4xds@proton.me>
2026-02-16 22:00:15 -0600

    program can now display total ram and ram usage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ansi_term::Colour::{Blue, Green, Red, Yellow};
 use ansi_term::Style;
 use std::arch::x86_64::CpuidResult;
-use std::io::stdout;
+use std::io::{stdout, Write};
 
 use crossterm::{
     cursor::MoveTo,
@@ -17,8 +17,10 @@ fn main() {
     //refresh all
     let mut sys = System::new_all();
     // non - changing vars
-    let total_ram = (sys.total_memory() / 1024 / 1024 / 1024);
-    let total_swap = sys.total_swap() / 1024 / 1024 / 1024;
+    let total_ram = (sys.total_memory() as f32) / (1024 as f32) / (1024 as f32) / (1024 as f32);
+    let total_swap = (sys.total_swap() as f32) / (1024 as f32) / (1024 as f32) / (1024 as f32);
+    let mut used_ram = 0;
+    let mut used_swap = 0;
     let total_cores: usize = sys.cpus().len();
     let cpu_name: String = sys
         .cpus()
@@ -42,15 +44,36 @@ fn main() {
     println!("   Total Cores: {} Cores", total_cores);
     for cpu in sys.cpus() {
         i += 1;
-        print!("   Core {} {}\n", i, cpu.frequency());
+        print!("   Core {} {}\n", i, cpu.cpu_usage());
     }
+    println!(
+        "MEMORY SECTION:              [ {:.2} GB physical memory ] [ {:.2} GB virtual memory]",
+        total_ram, total_swap
+    );
+    println!("   Physical memory used: {}", used_ram);
+    println!("   Virtual memory used:  {}", used_ram);
 
     loop {
         sys.refresh_all();
-        // VARIABLES:
-        let used_ram = sys.used_memory() / 1024 / 1024 / 1024;
-        let used_swap = sys.used_swap() / 1024 / 1024 / 1024;
+
+        show_ram(&sys, total_cores);
 
         std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
     }
+}
+
+// function to show ram and virtual ram
+fn show_ram(system: &System, total_cores: usize) {
+    let used_ram = (system.used_memory() as f32) / (1024 as f32) / (1024 as f32) / (1024 as f32);
+    stdout()
+        .execute(MoveTo(25, total_cores as u16 + 5))
+        .expect("Unable to update");
+    print!("{:.2}", used_ram);
+    stdout().flush().expect("Unable to update");
+    let used_swap = (system.used_swap() as f32) / (1024 as f32) / (1024 as f32) / (1024 as f32);
+    stdout()
+        .execute(MoveTo(25, total_cores as u16 + 6))
+        .expect("Unable to update");
+    print!("{:.2}", used_swap);
+    stdout().flush().expect("Unable to update");
 }
